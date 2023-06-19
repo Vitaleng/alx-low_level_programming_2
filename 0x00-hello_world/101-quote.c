06b4f6a208564f2c4a3766b15b580a72e6e86863
ineeleng <frecklesineeleng@gmail.com>
2023-06-19 11:27:40 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,10 @@
 #include <unistd.h>
 /**
  * main - Entry point
- *
- * Return: Always 1 (Success)
+ * Return:1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
