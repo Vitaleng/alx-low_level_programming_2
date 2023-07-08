6a214003c2e12bb6d77837c874ca50b2ef31d1c0
ineeleng <frecklesineeleng@gmail.com>
2023-07-08 07:19:51 -0700

    0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * _isupper - check the code for Holberton School students.
- *
- * @c: is an integer param
- *
- * Return: Always 0.
- */
-
+*_isupper - a function that checks for uppercase character
+*@c: single letter input
+*Return: 1 if int c is lowercase, 0 if otherwise
+*/
 
 int _isupper(int c)
 {
-
-	return (c >= 65 && c <= 90);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
-
